4410cb479a128c75fe3344a4ec361e73f150f50e
JakK <jakub.kuziola1@gmail.com>
2021-07-23 16:00:55 +0200

    23.07.2021 16:00 issues

--- src/Controller/issuesController.php
+++ src/Controller/issuesController.php
@@ -6,19 +6,17 @@ use App\Service\MarkdownHelper;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use App\Entity\Devices;
-use App\Entity\Companies;
-use ArrayAccess;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController extends AbstractController{
+class issuesController extends AbstractController{
 
     /**
      * Undocumented function
-     *@Route("/{$id}", name="device_issues")
+     *@Route("/issues/{id}", name="device_issues")
      */
-    public function show(EntityManagerInterface $entityManager)
+    public function show(EntityManagerInterface $entityManager, $id)
     {
         $repository=$entityManager->getRepository(Devices::class);
         $result=$repository->findAll();
@@ -26,7 +24,7 @@ class QuestionController extends AbstractController{
         $query
             ->select('u.device_id', 'u.name', 'u.company_name', 'u.expiry_date', 'u.status')
             ->from('Devices', 'u');
-        //dd($query);
-        return $this->render('question/homepage.html.twig', ['Devices' => $result,]);
+
+        return $this->render('question/issues.html.twig');
     }
 }
